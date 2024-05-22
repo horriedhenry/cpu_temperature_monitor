72d16e1ca2442366df8556d79d3b1dc2e49e61bd
horriedhenry <shivahenry33@gmail.com>
2024-05-23 01:32:28 +0530

    clang-format

--- main.cpp
+++ main.cpp
@@ -1,36 +1,33 @@
-#include <iostream>
+#include <chrono>
 #include <ctime>
+#include <fstream>
+#include <iostream>
 #include <map>
 #include <sstream>
-#include <vector>
-#include <fstream>
-#include <chrono>
 #include <thread>
+#include <vector>
 
-typedef struct Time
-{
+typedef struct Time {
     std::string hr;
     std::string mn;
     std::string sc;
     std::string am_pm;
-    std::string time() const
-    {
-        return hr + ":" + mn + ":" + sc + am_pm;
-    }
+    std::string time() const { return hr + ":" + mn + ":" + sc + am_pm; }
 } Time;
 
-Time get_time(std::tm* tm)
+Time get_time(std::tm *tm)
 {
     Time time;
-    int tm_hr {tm->tm_hour};
-    int tm_mn {tm->tm_min};
-    int tm_sc {tm->tm_sec};
+    int tm_hr{tm->tm_hour};
+    int tm_mn{tm->tm_min};
+    int tm_sc{tm->tm_sec};
 
     time.am_pm = (tm_hr < 12) ? " AM" : " PM";
 
     if (tm_hr == 0) {
         tm_hr = 12;
-    } else if (tm_hr > 12) {
+    }
+    else if (tm_hr > 12) {
         tm_hr -= 12;
     }
 
@@ -40,26 +37,25 @@ Time get_time(std::tm* tm)
     return time;
 }
 
-class Temps
-{
-public:
+class Temps {
+  public:
     std::vector<std::string> temp_rec_zone0;
     std::vector<std::string> temp_rec_zone5;
     std::vector<std::string> temp_rec_zone6;
     std::vector<std::string> temp_rec_zone7;
 
-    std::map<std::string, std::vector<std::string>*> temperature_records_in {
-        { "/sys/class/thermal/thermal_zone0/temp" , &temp_rec_zone0 },
-        { "/sys/class/thermal/thermal_zone5/temp" , &temp_rec_zone5 },
-        { "/sys/class/thermal/thermal_zone6/temp" , &temp_rec_zone6 },
-        { "/sys/class/thermal/thermal_zone7/temp" , &temp_rec_zone7 },
+    std::map<std::string, std::vector<std::string> *> temperature_records_in{
+        {"/sys/class/thermal/thermal_zone0/temp", &temp_rec_zone0},
+        {"/sys/class/thermal/thermal_zone5/temp", &temp_rec_zone5},
+        {"/sys/class/thermal/thermal_zone6/temp", &temp_rec_zone6},
+        {"/sys/class/thermal/thermal_zone7/temp", &temp_rec_zone7},
     };
 
-    std::map<std::string, std::vector<std::string>*> temperature_records_out {
-        { "./thermal/thermal_zone0" , &temp_rec_zone0 },
-        { "./thermal/thermal_zone5" , &temp_rec_zone5 },
-        { "./thermal/thermal_zone6" , &temp_rec_zone6 },
-        { "./thermal/thermal_zone7" , &temp_rec_zone7 },
+    std::map<std::string, std::vector<std::string> *> temperature_records_out{
+        {"./thermal/thermal_zone0", &temp_rec_zone0},
+        {"./thermal/thermal_zone5", &temp_rec_zone5},
+        {"./thermal/thermal_zone6", &temp_rec_zone6},
+        {"./thermal/thermal_zone7", &temp_rec_zone7},
     };
 
     void write_temperature_records()
@@ -67,15 +63,17 @@ public:
         std::ofstream file;
         std::stringstream str_stream;
 
-        for (auto itr = temperature_records_out.begin(); itr != temperature_records_out.end(); ++itr) {
-            for (const auto& str : *itr->second) {
+        for (auto itr = temperature_records_out.begin();
+             itr != temperature_records_out.end(); ++itr) {
+            for (const auto &str : *itr->second) {
                 str_stream << str << "\n";
             }
 
             file.open(itr->first, std::ios::app);
             if (file.is_open()) {
                 file << str_stream.str();
-            } else {
+            }
+            else {
                 return;
             }
             file.close();
@@ -84,9 +82,9 @@ public:
         }
     }
 
-    std::string log(std::string&& log_value, std::string&& time)
+    std::string log(std::string &&log_value, std::string &&time)
     {
-        std::string log {"[" + time + "] " + log_value};
+        std::string log{"[" + time + "] " + log_value};
         return log;
     }
 
@@ -95,10 +93,11 @@ public:
         std::ifstream file;
         std::string line;
 
-        std::time_t time_t {std::time(nullptr)};
-        std::tm* tm {std::localtime(&time_t)};
+        std::time_t time_t{std::time(nullptr)};
+        std::tm *tm{std::localtime(&time_t)};
 
-        for (auto itr = temperature_records_in.begin(); itr != temperature_records_in.end(); ++itr) {
+        for (auto itr = temperature_records_in.begin();
+             itr != temperature_records_in.end(); ++itr) {
             file.open(itr->first);
             if (file.is_open()) {
                 std::getline(file, line);
@@ -108,14 +107,15 @@ public:
 
                 line.clear();
                 file.close();
-            } else {
+            }
+            else {
                 return;
             }
         }
     }
 };
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
     Temps temps;
     int seconds_count{0};
